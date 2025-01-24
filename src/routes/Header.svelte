a9772ccc5d9f579c558c131f86e5e9285afa058a
Jonas_Jones <me@jonasjones.dev>
2025-01-25 00:52:09 +0100

    updated header code

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -16,15 +16,24 @@
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg>
 		<ul>
-			<li aria-current={page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
-			</li>
-			<li aria-current={page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
-			</li>
-			<li aria-current={page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
-			</li>
+			{#if ['/login', '/signup', '/register'].includes(page.url.pathname)}
+				<li aria-current={page.url.pathname === '/login' ? 'page' : undefined}>
+					<a href="\login{page.url.search}">Login</a>
+				</li>
+				<li aria-current={page.url.pathname === '/signup' ? 'page' : undefined}>
+					<a href="/signup{page.url.search}">Signup</a>
+				</li>
+			{:else}
+				<li aria-current={page.url.pathname === '/' ? 'page' : undefined}>
+					<a href="/">Home</a>
+				</li>
+				<li aria-current={page.url.pathname === '/about' ? 'page' : undefined}>
+					<a href="/about">About</a>
+				</li>
+				<li aria-current={page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
+					<a href="/sverdle">Sverdle</a>
+				</li>
+				{/if}
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
@@ -32,9 +41,6 @@
 	</nav>
 
 	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
-			<img src={github} alt="GitHub" />
-		</a>
 	</div>
 </header>
 
