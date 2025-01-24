ed332459d1b84e4574f54d94de0afc1359364f13
Jonas_Jones <me@jonasjones.dev>
2025-01-24 23:11:33 +0100

    fixed account backend url

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -7,7 +7,7 @@
 
     const handleLogin = async () => {
       try {
-        const response = await fetch('https://your-backend-api/login', {
+        const response = await fetch('https://accounts.jonasjones.dev/login', {
           method: 'POST',
           body: JSON.stringify({ email, password }),
           headers: {
