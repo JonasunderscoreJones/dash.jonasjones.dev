a4e42be9948e71324faefc3228e750e541245b54
Jonas_Jones <me@jonasjones.dev>
2025-03-02 21:44:44 +0100

    added proper footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Header from './Header.svelte';
+	import Footer from './Footer.svelte';
 	import '../app.css';
 
 	let { children } = $props();
@@ -12,11 +13,7 @@
 		{@render children()}
 	</main>
 
-	<footer>
-		<p>
-			visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to learn about SvelteKit
-		</p>
-	</footer>
+	<Footer />
 </div>
 
 <style>
